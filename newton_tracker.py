a77443acd44b026a7b7998f9d6f1b9e968a32224
Dalton Ronan <dronan@ualberta.ca>
2022-04-17 20:18:20 -0600

    prediction

--- newton_tracker.py
+++ newton_tracker.py
@@ -15,7 +15,9 @@ class NewtonTracker:
         self.image_window = "Source Image"
         self.templ_window = "Template Window"
         self.diff_window = "Diff Window"
-        self.match_method = 0
+        self.match_method = None
+        self.frame = 0
+        self.thresh = 3e-9
 
         self.cap = None
         self.prev_frame = None
@@ -40,9 +42,9 @@ class NewtonTracker:
     def load_video_sequence(self, filename: str):
         """Load and return the (.mp4) video specified by ``filename``.
 
-            @arg filename (str): The filename of the video to load and initialize.
+        @arg filename (str): The filename of the video to load and initialize.
 
-            @return cap (cv2.VideoCapture): The :py:class:`cv2.VideoCapture` object. 
+        @return cap (cv2.VideoCapture): The :py:class:`cv2.VideoCapture` object. 
         """
         self.cap = VideoCapture(filename)
 
@@ -51,7 +53,9 @@ class NewtonTracker:
     def select_template(self):
         """Select template from first frame of image sequence."""
         _, self.img = self.cap.read()
-        self.gray_img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+        self.frame += 1
+        # self.gray_img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+        self.gray_img = self.img
         self.roi = cv.selectROI('frame', self.img)
         self.templ = self.gray_img[
             int(self.roi[1]):int(self.roi[1]+self.roi[3]),
@@ -64,6 +68,8 @@ class NewtonTracker:
 
     def match_template(self):
         """Match the template image."""
+        # cv.imshow(self.diff_window, self.gray_img[self.p[1]:self.p[1] + self.templ.shape[0], self.p[0]: self.p[0] + self.templ.shape[1]] - self.templ)
+        # cv.waitKey(0)
         img_display = self.img.copy()
 
         method_accepts_mask = (
@@ -75,7 +81,17 @@ class NewtonTracker:
             result = cv.matchTemplate(
                 self.mask, self.templ, self.match_method)
         cv.normalize(result, result, 0, 1, cv.NORM_MINMAX, -1)
-        _minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(result, None)
+        minVal, _maxVal, minLoc, maxLoc = cv.minMaxLoc(result, None)
+
+        if (abs(minVal) > self.thresh):
+            self.predictUpdate()
+            cv.rectangle(img_display, self.p,
+                     (self.p[0] + self.templ.shape[1], self.p[1] + self.templ.shape[0]), (0, 255, 0), 2, 8, 0)
+            cv.imshow(self.image_window, img_display)
+            u = self.points[-1] - self.points[-2]
+            u = u.astype(int)
+            self.search_area = np.add(self.search_area, np.append(u, [0,0]))
+            return  
 
         if (self.match_method == cv.TM_SQDIFF or self.match_method == cv.TM_SQDIFF_NORMED):
             matchLoc = minLoc
@@ -84,11 +100,12 @@ class NewtonTracker:
 
         u = np.subtract(matchLoc, self.p-self.search_area[0:2])
         self.p = np.add(self.p, u)
-        self.addPoint(self.p)
 
+        self.addPoint(self.p)
         cv.rectangle(img_display, self.p,
                      (self.p[0] + self.templ.shape[1], self.p[1] + self.templ.shape[0]), (0, 0, 255), 2, 8, 0)
         cv.imshow(self.image_window, img_display)
+        cv.imshow('mask', self.mask)
         
         self.search_area = np.add(self.search_area, np.append(u, [0,0]))
 
@@ -96,11 +113,12 @@ class NewtonTracker:
         """Track the template image."""
         while self.cap.isOpened():
             ret, self.img = self.cap.read()
-
+            self.frame += 1
             if not ret:
                 break
 
-            self.gray_img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+            # self.gray_img = cv.cvtColor(self.img, cv.COLOR_BGR2GRAY)
+            self.gray_img = self.img
 
             self.mask = self.gray_img[int(self.search_area[1]):int(self.search_area[1]+self.search_area[3]),
                                       int(self.search_area[0]):int(self.search_area[0]+self.search_area[2])]
@@ -108,7 +126,17 @@ class NewtonTracker:
             
             cv.waitKey(1)
 
-    def plot(self):
+    def predictUpdate(self):
+        velocity = self.getVelocity()
+        x_prediction = self.points[-1][0] + velocity
+        z = np.polyfit(self.points[1:-1, 0], self.points[1:-1, 1], 2)
+        f = np.poly1d(z)
+        y_prediction = f(x_prediction)
+        self.p = [int(np.ceil(x_prediction)), int(np.ceil(y_prediction))]
+        print(self.p)
+        self.addPoint(self.p)
+
+    def plot(self, predict=False, *args):
         """Plot tracked points."""
         z = np.polyfit(self.points[1:-1, 0], np.negative(self.points[1:-1, 1]), 2)
         f = np.poly1d(z)
@@ -116,9 +144,24 @@ class NewtonTracker:
         y_new = f(x_new)
 
         plt.plot(self.points[1:-1, 0], np.negative(self.points[1:-1, 1]), 'o', x_new, y_new, markersize=3)
+
+        if predict:
+            velocity = self.getVelocity()
+            x_prediction = self.points[-1][0] + velocity
+            plt.plot(x_prediction, f(x_prediction), 'rx')
+
         plt.xlim([0, 1960])
         plt.ylim([-1060, 0])
         plt.show()
+
+    def getVelocity(self) -> float:
+        """Return the x velocity of the tracked object [pixels/frame].
+            
+        @return velocity (float): The x velocity
+        """
+        dx = self.points[-1][0] - self.points[1][0]
+        return dx/self.frame
+
 
     def close_tracker(self):
         """Close all windows and release the VideoCapture."""
